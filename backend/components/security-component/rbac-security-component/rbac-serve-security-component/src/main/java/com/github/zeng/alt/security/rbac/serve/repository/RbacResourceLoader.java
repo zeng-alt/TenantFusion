69f6d92e3f83e8c09d054ebf7bb74834f8bf15dd
zengjiajun <zeng15374396353@163.com>
2026-07-22 15:18:25 +0800

    feat(rbac): 实现其他的rbac和管理员权限控制

---
+++ backend/components/security-component/rbac-security-component/rbac-serve-security-component/src/main/java/com/github/zeng/alt/security/rbac/serve/repository/RbacResourceLoader.java
@@ -0,0 +1,16 @@
+package com.github.zeng.alt.security.rbac.serve.repository;
+
+import com.github.zeng.alt.security.api.Resource;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface RbacResourceLoader {
+
+    List<Resource> loadHttpResources(String username, String tenantName, List<String> authorities);
+
+    String loadPermissionByResource(String tenantName, String resourceKey);
+
+    Map<String, Set<String>> loadPermissions(List<String> authorities, String tenantName);
+}
